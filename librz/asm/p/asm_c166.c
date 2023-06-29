6141c7818fc00d5fc60d541e90c9af4660f07f14
frm dstryr <frmdstryr@protonmail.com>
2023-06-29 11:10:31 -0400

    Cleanup headers

--- librz/asm/p/asm_c166.c
+++ librz/asm/p/asm_c166.c
@@ -1,10 +1,7 @@
 // SPDX-FileCopyrightText: 2023 Jairus Martin <frmdstryr@protonmail.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
-#include <stdio.h>
-#include <string.h>
 #include <rz_types.h>
-#include <rz_lib.h>
 #include <rz_asm.h>
 
 #include "../arch/c166/c166_dis.h"
