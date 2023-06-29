0a990714b60006b531880980d96c6ea52341e544
frm dstryr <frmdstryr@protonmail.com>
2023-06-29 09:20:46 -0400

    Reformat and add support initial esfr support

--- librz/asm/p/asm_c166.c
+++ librz/asm/p/asm_c166.c
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Jairus Martin <frmdstryr@protonmail.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
-
 #include <stdio.h>
 #include <string.h>
 #include <rz_types.h>
@@ -12,27 +11,27 @@
 
 static int disassemble(RzAsm *a, RzAsmOp *op, const ut8 *buf, int len) {
 	struct c166_cmd cmd;
+	cmd.esfr = false; // TODO: Depends on ext instr, how can this be set?
 	int ret = c166_decode_command(buf, &cmd, len);
 	rz_strbuf_set(&op->buf_asm, sdb_fmt("%s %s", cmd.instr, cmd.operands));
 	return op->size = ret;
 }
 
-
 RzAsmPlugin rz_asm_plugin_c166 = {
-    .name = "c166",
-    .license = "LGPL3",
-    .desc = "Bosch/Siemens C166 disassembly plugin",
-    .arch = "c166",
-    .bits = 16,
-    .endian = RZ_SYS_ENDIAN_LITTLE,
-    .disassemble = &disassemble,
-    .cpus = "c167cr"
+	.name = "c166",
+	.license = "LGPL3",
+	.desc = "Bosch/Siemens C166 disassembly plugin",
+	.arch = "c166",
+	.bits = 16,
+	.endian = RZ_SYS_ENDIAN_LITTLE,
+	.disassemble = &disassemble,
+	.cpus = "c167cr"
 };
 
 #ifndef RZ_PLUGIN_INCORE
 RZ_API RzLibStruct rizin_plugin = {
-    .type = RZ_LIB_TYPE_ASM,
-    .data = &rz_asm_plugin_c166,
-    .version = RZ_VERSION
+	.type = RZ_LIB_TYPE_ASM,
+	.data = &rz_asm_plugin_c166,
+	.version = RZ_VERSION
 };
 #endif
